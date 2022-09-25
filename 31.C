b15e3a7e4682b817f58cedbd90fa7e80d0005a85
CodeWithAmann <113423671+CodeWithAmann@users.noreply.github.com>
2022-09-26 00:04:56 +0530

    Create 31.C

---
+++ 31.C
@@ -0,0 +1,35 @@
+//*************************** USING ITERATIVE METHOD *************************************
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n,i, fact=1;
+//     printf("Enter the number that's you want to find the factorial of them \n");
+//     scanf("%d", &n);
+//     for (i = 1; i <= n; i++)
+//     {
+//         fact = fact *i ;
+//     }
+//     printf("The factoraial of %d is %d",n,fact);
+//     return 0;
+// }
+
+//*********************************** using recursive method *******************************
+
+// #include<stdio.h>
+// int factorial ( int n)
+// {
+//     if (n<2)
+//     {
+//         return 1;
+//     }
+//    return n*factorial(n-1);
+// }
+// int main  ()
+// {
+//     int i;
+//     printf("Enter the number that's you want to finding them factoial \n");
+//     scanf("%d",&i);
+//     printf("The factoiral of %d is %d ", i, factorial(i));
+//     return 0;
+// }
